2ed289674ac1ecb92b0314aefc990498a65cb3f5
Rutger Hertoghe <rutgerh@hotmail.be>
2024-02-02 13:49:15 +0100

    Added initial code

---
+++ Code/RobotArm.ino
@@ -0,0 +1,43 @@
+#include "Servo.h"
+#include "Structs.h"
+#include "Controller.h"
+
+#define stickX A0 // GREEN WIRE is X
+#define stickY A1 // BLUE WIRE IS Y
+
+Servo baseJoint;
+Servo elbowJoint;
+Controller controller1{A0, A1, 1.f};
+
+unsigned long CalculateDeltaTime();
+
+void setup() {
+  Serial.begin(9600);
+  // SET CORRECT DIGITAL PWM!
+  baseJoint.attach(9);
+  elbowJoint.attach(8);
+  // IMPORTANT: 80 is the middle value for both arms
+  // SAFE VALUES BASE JOINT: 0 - 160
+  baseJoint.read(); // Lower values make the arm go forward
+  // SAFE VALUES ELBOW JOINT: 0 (full extension) - 150(BJ: 80)/ 160(BJ: 160)
+  elbowJoint.read(); // Lower values extend the arm, higher values flex the arm
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  auto deltaTimeMilli{CalculateDeltaTime()};
+  float deltaTime{float(deltaTimeMilli)/1000.f};
+
+  controller1.Update(deltaTime);
+}
+
+unsigned long CalculateDeltaTime()
+{
+  static unsigned long previousTime{};
+  unsigned long currentTime{millis()};
+  unsigned long deltaTime{currentTime - previousTime};
+  previousTime = currentTime;
+
+  return deltaTime;
+}
